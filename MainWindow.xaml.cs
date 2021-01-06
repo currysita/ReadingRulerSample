8f813556a2a96757b98672bc99bd82a1584d4e34
yasuda <yasuda@nextscape.net>
2021-01-06 15:15:46 +0900

    タスクトレイからの終了機能の実装 #2
    タスクトレイのアイコンの表示を実装
    タスクトレイにメニューを追加し、終了できるようにしました。
    アイコンはフリー素材を使用しています。

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,10 +21,70 @@ namespace ReadingRulerSample
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// タスクトレイの
+        /// </summary>
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
             //ここに画面初期化処理を組み込みます
+
+            //タスクトレイアイコンを初期化する
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Text = "リーディングルーラーサンプル";
+            _notifyIcon.Icon = new System.Drawing.Icon("app.ico");
+            //タスクトレイに表示する
+            _notifyIcon.Visible = true;
+
+            //アイコンにコンテキストメニュー「終了」を追加する
+            ContextMenuStrip menuStrip = new ContextMenuStrip();
+            ToolStripMenuItem exitItem = new ToolStripMenuItem();
+            exitItem.Text = "終了";
+            menuStrip.Items.Add(exitItem);
+            exitItem.Click += new EventHandler(exitItem_Click);
+            
+            _notifyIcon.ContextMenuStrip = menuStrip;
+
+            //タスクトレイアイコンのクリックイベントハンドラを登録する
+            _notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(_notifyIcon_MouseClick);
+        }
+
+        /// <summary>
+        /// 終了メニューのイベントハンドラ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _notifyIcon.Dispose();
+                System.Windows.Application.Current.Shutdown();
+            }
+            catch { }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _notifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    //ウィンドウを可視化
+                    Visibility = System.Windows.Visibility.Visible;
+                    WindowState = System.Windows.WindowState.Normal;
+                }
+            }
+            catch { }
         }
     }
 }
